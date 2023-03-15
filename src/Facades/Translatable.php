caa6cf4f4b8e3c7f1f9d1ae08476779e0bd27b3e
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-03-15 11:24:18 +0500

    adding comment

--- src/Facades/Translatable.php
+++ src/Facades/Translatable.php
@@ -4,6 +4,13 @@ namespace Khamdullaevuz\LaravelTranslation\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Khamdullaevuz\LaravelTranslation\Translatable translations()
+ * @method static \Khamdullaevuz\LaravelTranslation\Translatable getAttribute($key)
+ * @method static \Khamdullaevuz\LaravelTranslation\Translatable getTranslation($key, $locale)
+ * @see \Khamdullaevuz\LaravelTranslation\Translatable
+ */
+
 class Translatable extends Facade
 {
     protected static function getFacadeAccessor()
